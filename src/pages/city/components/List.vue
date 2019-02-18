6a5956918d46434654fc5fe2c4a0615fe6332ef8
wozien <571455773@qq.com>
2019-02-18 22:22:28 +0800

    add better-scroll

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list">
+    <div class="list" ref="wrapper">
         <div>
             <div class="area">
                 <div class="title border-topbottom">当前城市</div>
@@ -94,8 +94,13 @@
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
+
 export default {
-    name: 'CityList'
+    name: 'CityList',
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.wrapper);
+    }
 }
 </script>
 
